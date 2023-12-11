020576ba314ea3db19fd816939edabab2d70498d
Twisterday <garayda.rostik@gmail.com>
2023-12-10 21:13:53 -0600

    add slider to page

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$('.top__slider').slick({
+	dots: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+
+			}
+		},
+		{
+			breakpoint: 640,
+			settings: {
+				arrows: false
+			}
+		}
+	]
+});
