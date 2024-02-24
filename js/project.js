5432227f0507ab2f0368c6de98c9ed66a7b0d682
Twisterday <garayda.rostik@gmail.com>
2024-02-24 13:28:27 -0600

    Fix id problems - project block

--- js/project.js
+++ js/project.js
@@ -1,17 +1,35 @@
+// function openGallery(event, element) {
+// 	event.preventDefault();
+
+// 	var imgContainer = element.closest('.project__item-img');
+// 	var imgUrl = imgContainer.style.backgroundImage.slice(5, -2);
+
+// 	var overlay = document.getElementById('galleryOverlay');
+// 	var zoomedImg = document.getElementById('zoomedImg');
+
+// 	zoomedImg.src = imgUrl;
+// 	overlay.style.display = 'flex';
+// }
+
+// function closeGallery() {
+// 	var overlay = document.getElementById('galleryOverlay');
+// 	overlay.style.display = 'none';
+// }
+
 function openGallery(event, element) {
 	event.preventDefault();
 
 	var imgContainer = element.closest('.project__item-img');
 	var imgUrl = imgContainer.style.backgroundImage.slice(5, -2);
 
-	var overlay = document.getElementById('galleryOverlay');
-	var zoomedImg = document.getElementById('zoomedImg');
+	var overlay = document.querySelector('.gallery-overlay');
+	var zoomedImg = document.querySelector('.zoomed-img');
 
 	zoomedImg.src = imgUrl;
 	overlay.style.display = 'flex';
 }
 
 function closeGallery() {
-	var overlay = document.getElementById('galleryOverlay');
+	var overlay = document.querySelector('.gallery-overlay');
 	overlay.style.display = 'none';
 }
