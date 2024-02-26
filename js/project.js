06f9745118103b8764a19ad15292bb1f6de733a8
Twisterday <garayda.rostik@gmail.com>
2024-02-26 11:18:49 -0600

    Delete some comments

--- js/project.js
+++ js/project.js
@@ -1,21 +1,3 @@
-// function openGallery(event, element) {
-// 	event.preventDefault();
-
-// 	var imgContainer = element.closest('.project__item-img');
-// 	var imgUrl = imgContainer.style.backgroundImage.slice(5, -2);
-
-// 	var overlay = document.getElementById('galleryOverlay');
-// 	var zoomedImg = document.getElementById('zoomedImg');
-
-// 	zoomedImg.src = imgUrl;
-// 	overlay.style.display = 'flex';
-// }
-
-// function closeGallery() {
-// 	var overlay = document.getElementById('galleryOverlay');
-// 	overlay.style.display = 'none';
-// }
-
 function openGallery(event, element) {
 	event.preventDefault();
 
